c835875f0d2b8b44849903e1d81205bde4794488
Brian Neisler <neisler.brian@gmail.com>
2014-07-16 18:11:47 -0700

    Add lintbug support. Upgrade to bugpack 0.1.14. Bump version to 0.1.4. Lint fixes.

--- buildbug.js
+++ buildbug.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2014 airbug inc. http://airbug.com
+ *
+ * bugtrace may be freely distributed under the MIT license.
+ */
+
+
 //-------------------------------------------------------------------------------
 // Requires
 //-------------------------------------------------------------------------------
@@ -11,6 +18,7 @@ var buildbug            = require('buildbug');
 
 var buildProject        = buildbug.buildProject;
 var buildProperties     = buildbug.buildProperties;
+var buildScript         = buildbug.buildScript;
 var buildTarget         = buildbug.buildTarget;
 var enableModule        = buildbug.enableModule;
 var parallel            = buildbug.parallel;
@@ -26,6 +34,7 @@ var aws                 = enableModule("aws");
 var bugpack             = enableModule('bugpack');
 var bugunit             = enableModule('bugunit');
 var core                = enableModule('core');
+var lintbug             = enableModule("lintbug");
 var nodejs              = enableModule('nodejs');
 
 
@@ -33,9 +42,9 @@ var nodejs              = enableModule('nodejs');
 // Values
 //-------------------------------------------------------------------------------
 
-var version             = "0.1.3";
+var version             = "0.1.4";
 var dependencies        = {
-    bugpack: "0.1.12"
+    bugpack: "0.1.14"
 };
 
 
@@ -86,7 +95,6 @@ buildProperties({
             },
             sourcePaths: [
                 "../buganno/projects/buganno/js/src",
-                "../bugflow/projects/bugflow/js/src",
                 "../bugfs/projects/bugfs/js/src",
                 "../bugjs/projects/bugyarn/js/src",
                 "../bugmeta/projects/bugmeta/js/src",
@@ -102,7 +110,18 @@ buildProperties({
                 "./projects/bugtrace/js/test"
             ]
         }
-    }
+    },
+    lint: {
+        targetPaths: [
+            "."
+        ],
+        ignorePatterns: [
+            ".*\\.buildbug$",
+            ".*\\.bugunit$",
+            ".*\\.git$",
+            ".*node_modules$"
+        ]
+    },
 });
 
 
@@ -124,18 +143,34 @@ buildTarget('clean').buildFlow(
 buildTarget('local').buildFlow(
     series([
         targetTask('clean'),
+        targetTask('lint', {
+            properties: {
+                targetPaths: buildProject.getProperty("lint.targetPaths"),
+                ignores: buildProject.getProperty("lint.ignorePatterns"),
+                lintTasks: [
+                    "cleanupExtraSpacingAtEndOfLines",
+                    "ensureNewLineEnding",
+                    "indentEqualSignsForPreClassVars",
+                    "orderBugpackRequires",
+                    "orderRequireAnnotations",
+                    "updateCopyright"
+                ]
+            }
+        }),
         series([
             targetTask('createNodePackage', {
                 properties: {
                     packageJson: buildProject.getProperty("node.packageJson"),
-                    readmePath: buildProject.getProperty("node.readmePath"),
-                    sourcePaths: buildProject.getProperty("node.sourcePaths").concat(
-                        buildProject.getProperty("node.unitTest.sourcePaths")
-                    ),
-                    scriptPaths: buildProject.getProperty("node.scriptPaths").concat(
-                        buildProject.getProperty("node.unitTest.scriptPaths")
-                    ),
-                    testPaths: buildProject.getProperty("node.unitTest.testPaths")
+                    packagePaths: {
+                        "./": [buildProject.getProperty("node.readmePath")],
+                        "./lib": buildProject.getProperty("node.sourcePaths").concat(
+                            buildProject.getProperty("node.unitTest.sourcePaths")
+                        ),
+                        "./scripts": buildProject.getProperty("node.scriptPaths").concat(
+                            buildProject.getProperty("node.unitTest.scriptPaths")
+                        ),
+                        "./test": buildProject.getProperty("node.unitTest.testPaths")
+                    }
                 }
             }),
             targetTask('generateBugPackRegistry', {
@@ -194,6 +229,20 @@ buildTarget('local').buildFlow(
 buildTarget('prod').buildFlow(
     series([
         targetTask('clean'),
+        targetTask('lint', {
+            properties: {
+                targetPaths: buildProject.getProperty("lint.targetPaths"),
+                ignores: buildProject.getProperty("lint.ignorePatterns"),
+                lintTasks: [
+                    "cleanupExtraSpacingAtEndOfLines",
+                    "ensureNewLineEnding",
+                    "indentEqualSignsForPreClassVars",
+                    "orderBugpackRequires",
+                    "orderRequireAnnotations",
+                    "updateCopyright"
+                ]
+            }
+        }),
         parallel([
 
             //Create test node bugtrace package
@@ -302,3 +351,17 @@ buildTarget('prod').buildFlow(
         ])
     ])
 );
+
+
+//-------------------------------------------------------------------------------
+// Build Scripts
+//-------------------------------------------------------------------------------
+
+buildScript({
+    dependencies: [
+        "bugcore",
+        "bugflow",
+        "bugfs"
+    ],
+    script: "./lintbug.js"
+});
--- projects/bugtrace/js/test/BugTraceTests.js
+++ projects/bugtrace/js/test/BugTraceTests.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2014 airbug inc. http://airbug.com
+ *
+ * bugtrace may be freely distributed under the MIT license.
+ */
+
+
 //-------------------------------------------------------------------------------
 // Annotations
 //-------------------------------------------------------------------------------
@@ -20,18 +27,18 @@ require('bugpack').context("*", function(bugpack) {
     // BugPack
     //-------------------------------------------------------------------------------
 
-    var Class           = bugpack.require('Class');
-    var BugMeta         = bugpack.require('bugmeta.BugMeta');
-    var BugTrace        = bugpack.require('bugtrace.BugTrace');
-    var TestTag  = bugpack.require('bugunit.TestTag');
+    var Class       = bugpack.require('Class');
+    var BugMeta     = bugpack.require('bugmeta.BugMeta');
+    var BugTrace    = bugpack.require('bugtrace.BugTrace');
+    var TestTag     = bugpack.require('bugunit.TestTag');
 
 
     //-------------------------------------------------------------------------------
     // Simplify References
     //-------------------------------------------------------------------------------
 
-    var bugmeta         = BugMeta.context();
-    var test            = TestTag.test;
+    var bugmeta     = BugMeta.context();
+    var test        = TestTag.test;
 
 
     //-------------------------------------------------------------------------------
